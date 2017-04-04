0d4983116705f2fdf5cf65ae96dfb33ec50e682e
Kirill Tolkachev <lavcraft@gmail.com>
2017-04-04 10:32:23 +0700

    Fix jbaruch phrase

--- assistant/src/test/java/com/conference/spring/test/webassistant/controller/QuestionControllerTest.java
+++ assistant/src/test/java/com/conference/spring/test/webassistant/controller/QuestionControllerTest.java
@@ -3,28 +3,29 @@ package com.conference.spring.test.webassistant.controller;
 import com.conference.spring.test.webassistant.client.JBaruchClient;
 import com.conference.spring.test.webassistant.domain.Answer;
 import com.conference.spring.test.webassistant.domain.Question;
-import com.conference.spring.test.webassistant.domain.QuestionType;
 import com.conference.spring.test.webassistant.service.NotificationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.PostConstruct;
 
+import static com.conference.spring.test.webassistant.domain.QuestionType.JBARUCH;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
 
 /**
  * @author tolkv
@@ -47,21 +48,22 @@ public class QuestionControllerTest {
 
   @Test
   public void should_send_question() throws Exception {
-    doReturn(Answer.builder()
-        .answer("Нормально делай нормально будет")
+    Mockito.doReturn(Answer.builder()
+        .answer("Технологии идеальны, люди – ***ки")
         .build())
         .when(jBaruchClient).handleQuestion(anyObject());
 
-    ResponseEntity<Question> questionResponseEntity = testRestTemplate.postForEntity("/question", Question.builder()
-            .body("Что нужно сделать чтобы было артифактори работал нормально?")
+    ResponseEntity<Question> questionResponseEntity = testRestTemplate.postForEntity("/question",
+        Question.builder()
+            .body("Технологии для людей?")
             .build()
         , Question.class);
 
-    assertThat(questionResponseEntity.getStatusCode(), equalTo(HttpStatus.OK));
+    assertThat(questionResponseEntity.getStatusCode(), equalTo(OK));
 
     verify(notificationService, times(1)).notify(argumentCaptor.capture());
 
-    assertThat(argumentCaptor.getValue().getAnswer(), equalTo("Нормально делай нормально будет"));
+    assertThat(argumentCaptor.getValue().getAnswer(), equalTo("Технологии идеальны, люди – ***ки"));
   }
 
   @TestConfiguration
@@ -72,7 +74,7 @@ public class QuestionControllerTest {
     @PostConstruct
     public void init() {
       when(jBaruchClient.assistantName())
-          .thenReturn(QuestionType.JBARUCH);
+          .thenReturn(JBARUCH);
     }
   }
 }
